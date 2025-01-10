c4a39a719b2053a4ca7efe87509c15f5f6ebd429
BT_1N3_02 <GlooboGlabba@bths.edu>
2025-01-10 11:01:12 -0500

    Modified print statement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Our repo");
-        System.out.println("Hello");
+        System.out.println("The king Varun");
     }
 }
