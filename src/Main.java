4d376bf9b4e155f3cdaf6eac5fd4da62bb292f50
BT_1N3_01 <sample@bths.edu>
2025-01-10 10:57:45 -0500

    Added print

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Our repo");
-        System.out.println("The king brandon");
+        System.out.println("Hello");
     }
 }
